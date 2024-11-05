7d5204594dee34f3ecd670a5aff5ca120c18373b
Матвей <matvei@MacBook-Pro-Matvej.local>
2024-11-05 19:09:14 +0300

    New commit

--- app/src/main/java/hexlet/code/app/model/User.java
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -1,7 +1,14 @@
 package hexlet.code.app.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+
+
 import lombok.Getter;
 import lombok.Setter;
 
