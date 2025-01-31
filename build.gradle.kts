2ee6862c7515fd956e6fe831ba14ba561f1f98f3
Матвей <matvei@MacBook-Pro-Matvej.local>
2025-01-31 19:23:36 +0300

    New commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
-application { mainClass.set("hexlet.code.AppApplication") }
+application { mainClass.set("hexlet.code.app.AppApplication") }
 
 java {
 	toolchain {
